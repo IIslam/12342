ad872f150bb71612d44b8d2b8503ce0e98aeb8b6
jonathan.lundmark <jonathan.lundmark@eqtpartners.com>
2020-11-17 13:48:39 +0100

    add db seeds

--- init.sql
+++ init.sql
@@ -46,9 +46,26 @@ create table fund_financial
 		constraint fund_financial_fund_id_fk
 			references fund,
 	type financial_type not null,
-	created_at timestamp default now(),
+	created_at timestamp default now()
 )
 ;
 
 alter table fund_financial owner to u1
 ;
+
+-- Seeds
+
+INSERT INTO fund (id, name, description) VALUES (1, 'EQT X', 'Encompasses investments mainly within the public value segment');
+INSERT INTO fund (id, name, description) VALUES (2, 'EQT Y', 'Encompasses investments mainly within the real estate segment');
+
+INSERT INTO "user" (email, first_name, last_name, fund_fk) VALUES ('jane@doe.eqt.com', 'Jane', 'Doe', 1);
+INSERT INTO "user" (email, first_name, last_name, fund_fk) VALUES ('john@doe.eqt.com', 'John', 'Doe', 2);
+
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (35000000, 1, 'DEBIT', '2020-11-15 12:29:58.886418', 'SEK');
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (20000000, 1, 'CREDIT', '2020-11-12 12:29:58.886418', 'SEK');
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (400000000, 1, 'CREDIT', '2020-11-17 12:29:58.886418', 'SEK');
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (25005000, 1, 'DEBIT', '2020-11-19 12:29:58.886418', 'SEK');
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (3000000, 1, 'DEBIT', '2020-11-15 12:29:58.886418', 'SEK');
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (70000000, 2, 'CREDIT', '2020-11-11 12:29:58.886418', 'SEK');
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (450000000, 2, 'CREDIT', '2020-11-15 12:29:58.886418', 'SEK');
+INSERT INTO fund_financial (amount, fund_fk, type, created_at, currency) VALUES (35000000, 2, 'DEBIT', '2020-11-20 12:29:58.886418', 'SEK');
