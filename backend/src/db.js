c88b93a1a6b5274d417ecce37a566d265aac3c64
jonathan.lundmark <jonathan.lundmark@eqtpartners.com>
2020-11-17 21:03:05 +0100

    dockerize backend frontend and db

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,16 +1,16 @@
-const {Pool} = require('pg')
-const dbCredentials = require('../secrets/db-credentials.json')
+const { Pool } = require('pg')
+const { database, user, password, host } = require('../secrets/db-credentials.json')
 
 class Db {
-  constructor () {
-    this.pool = new Pool(dbCredentials)
+  constructor() {
+    this.pool = new Pool({ host: process.env.DB_HOST || host, database, user, password })
     this.init()
     this.pool.on('error', (err, client) => {
       this.init()
     })
   }
 
-  async init () {
+  async init() {
     try {
       const client = await this.pool.connect()
       await client.query('SELECT NOW()')
@@ -21,9 +21,9 @@ class Db {
     }
   }
 
-  async query (q) {
+  async query(q) {
     try {
-      const {rows} = await this.pool.query(q)
+      const { rows } = await this.pool.query(q)
       return rows
     } catch (e) {
       console.log(`Error querying db q:${q} e:${e}`)
