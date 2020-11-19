f5d7eb5671f6ac403ef576f4466358a50de5b85b
jonathan.lundmark <jonathan.lundmark@eqtpartners.com>
2020-11-19 10:57:40 +0100

    improve instructions and add comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,61 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react'
+import './App.css'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       users: null,
       funds: null,
       financials: null,
-    };
+    }
   }
 
-  getFundData() {
+  /**
+   * Head over to the backend to remove, add or modify these endpoints to your liking.
+   */
+  getFundData () {
     fetch('http://localhost:9090/v1/users')
       .then(response => response.json())
-      .then(data => this.setState({ users: data }));
+      .then(data => this.setState({users: data}))
     fetch('http://localhost:9090/v1/funds')
       .then(response => response.json())
-      .then(data => this.setState({ funds: data }));
+      .then(data => this.setState({funds: data}))
     fetch('http://localhost:9090/v1/fund_financials')
       .then(response => response.json())
-      .then(data => this.setState({ financials: data }));
+      .then(data => this.setState({financials: data}))
   }
 
-  render() {
-    const { users, funds, financials } = this.state;
+  /**
+   * TODO: Give some love
+   */
+  render () {
+    const {users, funds, financials} = this.state
     return (
       <div className="App">
         <h1 className="header">Fund Data Visualisation</h1>
         <button className="fetch-button" onClick={() => this.getFundData()}>FETCH DATA</button>
         <div className="fund-data-container">
           {users &&
-            <>
-              <h2>User Data</h2>
-              {users.map(u => <p>{JSON.stringify(u)}</p>)}
-            </>
+          <>
+            <h2>User Data</h2>
+            {users.map(u => <p>{JSON.stringify(u)}</p>)}
+          </>
           }
           {funds &&
-            <>
-              <h2>Fund Data</h2>
-              {funds.map(f => <p>{JSON.stringify(f)}</p>)}
-            </>
+          <>
+            <h2>Fund Data</h2>
+            {funds.map(f => <p>{JSON.stringify(f)}</p>)}
+          </>
           }
           {financials &&
-            <>
-              <h2>Financial Data</h2>
-              {financials.map(f => <p>{JSON.stringify(f)}</p>)}
-            </>
+          <>
+            <h2>Financial Data</h2>
+            {financials.map(f => <p>{JSON.stringify(f)}</p>)}
+          </>
           }
         </div>
       </div>
-    );
+    )
   }
 }
--- frontend/src/Instructions.js
+++ frontend/src/Instructions.js
@@ -1,56 +1,58 @@
-import React from "react";
-import logo from "./eqt-logo.png";
-import "./Instructions.css";
+import React from 'react'
+import logo from './eqt-logo.png'
+import './Instructions.css'
 
-export default function Instructions() {
+export default function Instructions () {
   return (
     <div className="Instructions">
-      <img src={logo} className="Instructions-logo" alt="Logo" />
+      <img src={logo} className="Instructions-logo" alt="Logo"/>
       <h1>Hello, dear future EQT Fullstack Engineer!</h1>
       <p>
         This is a code test that is meant to test your creativity and problem
         solving skills.
       </p>
       <p>
-        This repository is a barebones setup with a Node backend and a React
+        This repository is a barebones setup with a Node backend, a Postgres database and a React
         frontend. If you really want to, you can tear it all down and start from
         scratch, but this is meant to get you started immediately.
       </p>
       <div className="challenge-container">
         <h2>The Challenge: Fund Data Visualisation</h2>
         <p>
-          <strong>Baseline:</strong>
+          <strong>Baseline</strong>
           <p>Your job is to provide an overview for an
-        investors fund investments. Once your happy with the{" "}
-            <strong>baseline </strong>
+            investors fund investments. Once your happy with the<strong> baseline </strong>
             pick one of the below tracks (or both if you have the time):
           </p>
         </p>
         <p>
-          <strong>Backend Track:</strong>
+          <strong>Backend Track</strong>
         </p>
         <p>A user has up until recently only been allowed to invest in 1 fund,
-            but due to recent legal changes they are now allowed to invest in
-            multiple funds. Make sure the data model and returned data supports
+          but due to recent legal changes they are now allowed to invest in
+          multiple funds. Make sure the data model and returned data supports
           this new legal requirement!</p>
         <p>
-          <strong>Frontend Track:</strong>
+          <strong>Frontend Track</strong>
         </p>
-        <p> Get creative with the visualisation, make use of Charts, Search,
-            Filters or anything you find interesting.      Examples of what can be interesting: Total revenue, performance over
-          time, comparison of funds.</p>
+        <p> Get creative with the visualisation, make use of a Table, Charts, Search,
+          Filters or anything you find interesting. Examples of what can be interesting from an analytics perspective:
+          Total revenue, performance over time, comparison of funds or anything else you can think of.</p>
       </div>
-      <p> We want you to{" "}
-        <strong>
-          explore and create a table, chart or graph of any aspect of the data. Use any
-          charting library you want or whip something up yourself if you prefer. Points for creativity, both in aesthetics and implementation.
-        </strong>
-      </p>
+      <strong>Feel free to alter any aspect of this application!</strong>
+      <p>Focus on frontend or backend or both.</p>
+      <p>Change the code structure in any way you prefer.</p>
+      <p>Alter the data model if you want.</p>
+      <p>introduce any libraries you feel would help you.</p>
+      <p>Add any relevant tests (frontend and/or backend) if you wish.</p>
+      <p>Dazzle us.</p>
+      <br/>
       <p>
-        Press the button below to fetch and present the data. How will <em>you</em> make
+        Press the button below to fetch data from the backend see what you initially have to work with. How
+        will <strong>YOU</strong> make
         it more fun?
       </p>
-      <p style={{ color: "#ff6500" }}>Good Luck!</p>
+      <p style={{color: '#ff6500'}}>Good Luck!</p>
     </div>
-  );
+  )
 }
