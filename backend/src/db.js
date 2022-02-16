723b8bc4f34d733342314e6555ccdb311d1e0b4a
Islam Gad <igad@smartchargetech.com>
2022-02-16 13:13:28 +0200

    [Use] .env instead of secrets

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,36 +1,41 @@
-const { Pool } = require('pg')
-const { database, user, password, host } = require('../secrets/db-credentials.json')
+const { Pool } = require("pg");
+const { database, user, password, host } = process.env;
 
 class Db {
   constructor() {
-    this.pool = new Pool({ host: process.env.DB_HOST || host, database, user, password })
-    this.init()
-    this.pool.on('error', (err, client) => {
-      this.init()
-    })
+    this.pool = new Pool({
+      host: process.env.DB_HOST || host,
+      database,
+      user,
+      password,
+    });
+    this.init();
+    this.pool.on("error", (err, client) => {
+      this.init();
+    });
   }
 
   async init() {
     try {
-      const client = await this.pool.connect()
-      await client.query('SELECT NOW()')
-      client.release()
-      console.log('🚀 DB connection is up')
+      const client = await this.pool.connect();
+      await client.query("SELECT NOW()");
+      client.release();
+      console.log("🚀 DB connection is up");
     } catch (e) {
-      console.log(`Error connecting to db e:${e}`)
+      console.log(`Error connecting to db e:${e}`);
     }
   }
 
   async query(q) {
     try {
-      const { rows } = await this.pool.query(q)
-      return rows
+      const { rows } = await this.pool.query(q);
+      return rows;
     } catch (e) {
-      console.log(`Error querying db q:${q} e:${e}`)
-      throw e
+      console.log(`Error querying db q:${q} e:${e}`);
+      throw e;
     }
   }
 }
 
-const db = new Db()
-module.exports = db
+const db = new Db();
+module.exports = db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,34 @@
-const express = require('express')
-const db = require('./db')
-const app = express()
+var dotenv = require("dotenv");
+var dotenvExpand = require("dotenv-expand");
+
+var myEnv = dotenv.config();
+dotenvExpand(myEnv);
+
+const express = require("express");
+const db = require("./db");
+const app = express();
 
 app.use(function (req, res, next) {
-  const origin = req.get('origin')
-  res.header('Access-Control-Allow-Origin', origin)
-  next()
-})
+  const origin = req.get("origin");
+  res.header("Access-Control-Allow-Origin", origin);
+  next();
+});
 
-app.get('/v1/funds', async function (req, res, next) {
-  const funds = await db.query(`SELECT * FROM fund`)
-  console.log(funds)
-  res.status(200).json(funds)
-})
-app.get('/v1/users', async function (req, res, next) {
-  const users = await db.query(`SELECT * FROM "user"`)
-  res.status(200).json(users)
-})
-app.get('/v1/fund_financials', async function (req, res, next) {
-  const fund_financials = await db.query(`SELECT * FROM fund_financial`)
-  res.status(200).json(fund_financials)
-})
+app.get("/v1/funds", async function (req, res, next) {
+  const funds = await db.query(`SELECT * FROM fund`);
+  res.status(200).json(funds);
+});
+app.get("/v1/users", async function (req, res, next) {
+  const users = await db.query(`SELECT * FROM "user"`);
+  res.status(200).json(users);
+});
+app.get("/v1/fund_financials", async function (req, res, next) {
+  const fund_financials = await db.query(`SELECT * FROM fund_financial`);
+  res.status(200).json(fund_financials);
+});
 
 // Start server
-const port = 9090
+const port = 9090;
 app.listen(port, () => {
-  console.log('🚀 Server is up on port', port)
-})
+  console.log("🚀 Server is up on port", port);
+});
